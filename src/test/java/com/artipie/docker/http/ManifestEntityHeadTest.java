d1380e8319579bde1ec3bac63d19f99f35e48ed7
olenagerasimova <olena.gerasimova@gmail.com>
2021-04-22 16:57:11 +0300

    Parse multi-value Accept header

--- src/test/java/com/artipie/docker/http/ManifestEntityHeadTest.java
+++ src/test/java/com/artipie/docker/http/ManifestEntityHeadTest.java
@@ -135,7 +135,8 @@ class ManifestEntityHeadTest {
         Headers() {
             super(
                 new Headers.From(
-                    new Header("Accept", "application/vnd.docker.distribution.manifest.v2+json")
+                    // @checkstyle LineLengthCheck (1 line)
+                    new Header("Accept", "application/vnd.docker.distribution.manifest.v2+json, application/xml;q=0.9, image/*")
                 )
             );
         }
